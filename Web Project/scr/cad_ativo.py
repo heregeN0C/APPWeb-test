57575aaf838efd9c04feed35773ce04db6a1b69b
heregeN0C <64712728+heregeN0C@users.noreply.github.com>
2022-08-08 16:49:07 -0300

    Delete Web Project directory

--- Web Project/scr/cad_ativo.py
+++
@@ -1,5 +0,0 @@
-from psycopg2 import _psycopg
-
-conn = _psycopg._connect("dbname=db_gerencia user=alyson password=Alys0n!@")
-
-cursor = co
--- Web Project/scr/model/apiIXC.py
+++
@@ -1,6 +0,0 @@
-from io import *
-
-host = 'https://fibraturbo.com.br/webservice/v1'
-token = '20:93f47ef2734cafadde3310f5d6075fb75f5c871b10ae3cc58303622d1262e320'
-selfSigned = false
-api = new IXCsoft\WebserviceClient(host, token, selfSigned)
